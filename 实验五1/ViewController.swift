afb230d7a09c12e660ff3cd4873ae72d6098c846
student <student@imac-19.local>
2018-12-18 13:42:05 +0800

    finish

--- 实验五1/ViewController.swift
+++ 实验五1/ViewController.swift
@@ -13,6 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = FileManager.default
+        let document = manager.urls(for: .documentDirectory, in: .userDomainMask).first?.path
+        let file = document?.appending("/image") //图片文件夹
+        
+        //print(document!) //输出Document路径，方便查看
+        if manager.fileExists(atPath: file!) {  //若该文件夹存在
+            let image = file?.appending("/bd.png")  //图片文件
+            if manager.fileExists(atPath: image!) {  //若该文件存在，则显示到界面上
+                do{
+                    let data = try Data(contentsOf: URL(fileURLWithPath: image!))
+                    let img = UIImage(data: data)
+                    let imageView = UIImageView(image: img)
+                    imageView.frame = CGRect(x: 0, y: 100, width: 400, height: 300)
+                    self.view.addSubview(imageView)
+                } catch {
+                    print(error)
+                }
+            } else {  //若不存在，则从网络下载一个图片并保存为图片文件
+                let url = URL(string: "http://img4.imgtn.bdimg.com/it/u=4045428715,2766082195&fm=26&gp=0.jpg")
+                do{
+                    let data = try Data(contentsOf: url!)
+                    try data.write(to: URL(fileURLWithPath: image!), options: .atomicWrite)
+                    print("文件不存在，已成功创建文件")
+                } catch {
+                    print(error)
+                }
+            }
+        } else {  //若该文件夹不存在，则创建该文件夹
+            do {
+                try manager.createDirectory(atPath: file!, withIntermediateDirectories: true, attributes: nil)
+                print("文件夹不存在，已成功创建文件夹")
+            } catch {
+                print(error)
+            }
+        }
     }
 
 
